595686b5608263c443bac8fdbb756fb0dfe7d3bb
krincee <krincemakhoyi@gmail.com>
2026-09-18 16:57:30 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  // Optional: leave blank and use「連接資料庫」inside the app instead.
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxQRkQab9BU-zyBUpk_YL3L6YKT-VyvmYtMI4fnv9YfBFnXiFq7XYTsimFTuvgPYm-1/exec",
-  TMDB_IMAGE_BASE: "https://image.tmdb.org/t/p/",
-  DEFAULT_REGION: "HK",
-  DEFAULT_LANGUAGE: "zh-HK"
-};
